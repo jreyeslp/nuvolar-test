c69a6330e78331296696bc596a1e32bd89dcb67a
Javier de los Reyes <jreyeslp@gmail.com>
2021-03-02 19:27:56 +0100

    Duplicate code to trigger Sonar analysis

--- src/app/common/interfaces/github-data.ts
+++ src/app/common/interfaces/github-data.ts
@@ -1,15 +1,22 @@
 export interface GitHubUser {
-    login?: string,
-    avatar_url?: string,
-    name?: string
-    public_repos?: number,
-    followers?: number,
-    repoList?: GitHubRepo[]
+	login?: string,
+	avatar_url?: string,
+	name?: string
+	public_repos?: number,
+	followers?: number,
+	repoList?: GitHubRepo[]
 }
 
 export interface GitHubRepo {
-    name: string,
-    private: boolean,
-    description: string,
-    html_url: string
+	name: string,
+	private: boolean,
+	description: string,
+	html_url: string
+}
+
+export interface GitHubRepoCopy {
+	name: string,
+	private: boolean,
+	description: string,
+	html_url: string
 }
