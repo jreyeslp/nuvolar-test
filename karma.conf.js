00845b94316d626cfb77d09f99c9242a60721ff6
Javier de los Reyes <jreyeslp@gmail.com>
2021-03-02 15:41:40 +0100

    Update files for SQ

--- karma.conf.js
+++ karma.conf.js
@@ -3,39 +3,39 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('karma-sonarqube-unit-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage-istanbul-reporter"),
+      require("karma-sonarqube-unit-reporter"),
+      require("@angular-devkit/build-angular/plugins/karma"),
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/nuvolar-test'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+      dir: require("path").join(__dirname, "./coverage/nuvolar-test"),
+      reports: ["html", "lcovonly", "text-summary"],
+      fixWebpackSourcePaths: true,
     },
-    reporters: ['progress', 'kjhtml', 'sonarqubeUnit'],
+    reporters: ["progress", "kjhtml", "sonarqubeUnit"],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ["Chrome"],
     singleRun: false,
     restartOnFileChange: true,
     sonarQubeUnitReporter: {
-      sonarQubeVersion: 'LATEST',
-      outputFile: 'reports/ut_report.xml',
+      sonarQubeVersion: "LATEST",
+      outputFile: "reports/ut_report.xml",
       overrideTestDescription: true,
-      testPaths: ['./src'],
-      testFilePattern: '.spec.ts',
-      useBrowserName: false
-    }
+      testPaths: ["./src"],
+      testFilePattern: ".spec.ts",
+      useBrowserName: false,
+    },
   });
 };
